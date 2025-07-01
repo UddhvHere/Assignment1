d37fac26547888c9cd0a870828c3d719df1e42f3
UddhvHere <uddhavbhardwaj02@gmail.com>
2025-07-01 17:17:17 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+a=input('Enter the first number: ')
+a=float(a)
+b=input('Enter the second number: ')
+b=float(b)
+c=a+b
+c=float(c)
+d=a-b
+d=float(d)
+e=a*b
+e=float(e)
+f=a/b
+f=float(f)
+print('Addition: ' ,c)
+print('Subtraction: ' ,d)
+print('Multiplication: ' ,e)
+print('Division: ' ,f)
